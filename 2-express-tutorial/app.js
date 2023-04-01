3f439475b98d8c0c8e6aefd02f515053fbc30aeb
vivekranjan4256 <vivekranjan4256@gmail.com>
2023-04-01 09:40:27 +0530

    commit

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,71 +1,44 @@
 const express = require('express')
 const app = express()
-const {products} = require('./data')
+let { people } = require('./data')
+
+//static assets
+app.use(express.static('./methods-public'))
+
+//parse form data 
+app.use(express.urlencoded({extended: false}))
+//parse json data
+app.use(express.json())
 
 
 
-app.get('/',(req,res)=>{
-    res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
+app.get('/api/people',(req,res) =>{
+    res.status(200).json({success:true, data:people})
 })
 
-app.get('/api/products',(req,res)=>{
-    const newProducts = products.map((products)=>{
-        const {id,name,image} = products
-        return {id,name,image}
-    })
-    res.json({newProducts})
-})
-
-app.get('/api/products/:productID',(req,res)=>{
-    // console.log(req)
-    // console.log(req.params)
-    const {productID} = req.params
-    const singleProdcut = products.find((product) => product.id === Number(productID))
-    if(!singleProdcut)
-        res.status(404).send('Product does not exist')
-    res.json(singleProdcut)
-})
-
-app.get('/api/products/:productID/reviews/:reviewID',(req,res)=>{
-    console.log(req.params)
-    res.send('Hello World')
-})
-
-
-
-app.get('/api/v1/query',(req,res)=>{
-    // console.log(req.query)
-    const{ search, limit} = req.query
-    let sortedProducts = [...products]
-
-if(search){
-    sortedProducts = sortedProducts.filter((product)=>{
-        return product.name.startsWith(search)
-    })
-}
-
-if(limit){
-    sortedProducts=sortedProducts.slice(0,Number(limit))
-}
-if(sortedProducts.length<1){
-    // res.status(200).send('no products matched your search')
-    return res.status(200).json({success:true,data:[]})
-}
-res.status(200).json(sortedProducts)
-    // res.send('Hello World')
+//for javascript post
+app.post('/api/people',(req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(404).json({success:false, msg:'please provide name value'})
+    }
+    res.status(201).json({success:true,person:name})
 })
 
 
 
 
+//for html post
+app.post('/login',(req,res)=>{
+    const {name}= req.body
+    if(name){
+        res.status(200).send(`Welcome ${name}`)
+    }
+    else
+    res.status(401).send('Please Provide Credentials')
+})
 
 
-
-
-
-
-
-
-app.listen(5000,()=>{
-    console.log('Server is listening on port 5000...')
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000')
 })
